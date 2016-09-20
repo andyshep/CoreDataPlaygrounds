c331d98abf9280ebeadfc6037ca6f825d8b9c98d
Andrew Shepard <shep.andy@gmail.com>
2016-09-20 09:12:48 -0700

    Remove experimental stack

--- events-ios.playground/Contents.swift
+++ events-ios.playground/Contents.swift
@@ -6,85 +6,51 @@ import PlaygroundSupport
 
 let frame = CGRect(x: 0, y: 0, width: 320, height: 480)
 
-class Something {
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-        let container = NSPersistentContainer(name: "Model")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
+class ViewController: UIViewController {
+    
+    let dataSource: DataSource
+    
+    init(dataSource: DataSource) {
+        self.dataSource = dataSource
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Events"
+        
+        self.dataSource.tableView = self.tableView
+        self.view.addSubview(self.tableView)
+        
+        let item = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(handleAddButton))
+        self.navigationItem.rightBarButtonItem = item
+    }
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: frame)
+        return tableView
     }()
+    
+    func handleAddButton(_ sender: AnyObject) {
+        let context = self.dataSource.context
+        let entity = NSEntityDescription.insertNewObject(forEntityName: "Event", into: context)
+        entity.setValue(Date(), forKey: "timestamp")
+        
+        try! context.save()
+    }
 }
 
-let something = Something()
-let container = something.persistentContainer
+let context = try createManagedObjectContext()
+let dataSource = DataSource(context: context)
 
+let viewController = ViewController(dataSource: dataSource)
+let navController = UINavigationController(rootViewController: viewController)
 
+navController.view.frame = frame
 
-//class ViewController: UIViewController {
-//    
-//    let dataSource: DataSource
-//    
-//    init(dataSource: DataSource) {
-//        self.dataSource = dataSource
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        self.title = "Events"
-//        
-//        self.dataSource.tableView = self.tableView
-//        self.view.addSubview(self.tableView)
-//        
-//        let item = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(handleAddButton))
-//        self.navigationItem.rightBarButtonItem = item
-//    }
-//    
-//    lazy var tableView: UITableView = {
-//        let tableView = UITableView(frame: frame)
-//        return tableView
-//    }()
-//    
-//    func handleAddButton(_ sender: AnyObject) {
-//        let context = self.dataSource.context
-//        let entity = NSEntityDescription.insertNewObject(forEntityName: "Event", into: context)
-//        entity.setValue(Date(), forKey: "timestamp")
-//        
-//        try! context.save()
-//    }
-//}
-//
-//let context = try createManagedObjectContext()
-//let dataSource = DataSource(context: context)
-//
-//let viewController = ViewController(dataSource: dataSource)
-//let navController = UINavigationController(rootViewController: viewController)
-//
-//navController.view.frame = frame
-//
-//PlaygroundPage.current.liveView = navController.view
+PlaygroundPage.current.liveView = navController.view
--- events-ios.playground/Sources/CoreDataHelpers.swift
+++ events-ios.playground/Sources/CoreDataHelpers.swift
@@ -1,24 +1,24 @@
-//import CoreData
-//
-//public enum CoreDataError: ErrorProtocol {
-//    case modelNotFound
-//    case modelNotCreated
-//}
-//
-//public func createManagedObjectContext() throws -> NSManagedObjectContext {
-//    guard let modelURL = Bundle.main().urlForResource("Model", withExtension: "momd") else {
-//        throw CoreDataError.modelNotFound
-//    }
-//    
-//    guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
-//        throw CoreDataError.modelNotCreated
-//    }
-//    
-//    let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
-//    try psc.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
-//    
-//    let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-//    context.persistentStoreCoordinator = psc
-//    
-//    return context
-//}
+import CoreData
+
+public enum CoreDataError: Error {
+    case modelNotFound
+    case modelNotCreated
+}
+
+public func createManagedObjectContext() throws -> NSManagedObjectContext {
+    guard let modelURL = Bundle.main.url(forResource: "Model", withExtension: "momd") else {
+        throw CoreDataError.modelNotFound
+    }
+    
+    guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
+        throw CoreDataError.modelNotCreated
+    }
+    
+    let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
+    try psc.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+    
+    let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    context.persistentStoreCoordinator = psc
+    
+    return context
+}
